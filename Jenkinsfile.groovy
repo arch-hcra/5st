d6d2bd7d96e358afd95a6fe48495b7fb02ed802c
archhcra <bomcom@bk.ru>
2025-05-13 17:09:44 +0300

    4_commit

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,27 +1,28 @@
 pipeline {
-    agent any
-
+    agent any 
+    
     stages {
-        stage('Clone Repository') {
+        stage('Clone') {
             steps {
-                git 'https://github.com/arch-hcra/5st.git'
+                git branch: 'main', url: 'https://github.com/arch-hcra/5st.git' 
             }
         }
-        stage('Install Dependencies') {
-            steps {
-                sh 'pip install -r requirements.txt' // Убедитесь, что у вас есть файл requirements.txt
-            }
-        }
+
         stage('Run Tests') {
             steps {
-                sh 'python -m pytest test_login.py'
+                bat 'pytest test_login.py'
             }
         }
     }
-
     post {
         always {
-            junit '**/test-*.xml' // Если вы хотите собирать отчеты о тестах
+            script {
+                if (currentBuild.result == 'SUCCESS') {
+                    echo 'Yes complited'
+                } else {
+                    echo 'No complited'
+                }
+            }
         }
     }
 }
